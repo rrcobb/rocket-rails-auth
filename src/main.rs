bcba6a5872027f4864e2be0a08a3e50d2586ed6a
rob cobb <rwcobbjr@gmail.com>
2021-06-29 17:07:20 -0400

    rendering basic templates

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,57 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+use rocket::request::Form;
+use rocket::response::Redirect;
+use rocket::http::{Cookie, Cookies};
+use rocket_contrib::templates::Template;
+use serde::{Serialize};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-// get /login, render login template
-// get /signup, render signup template
-// post /login, check credentials, issue cookie on good credentials, redirect
+#[derive(FromForm, Serialize)]
+struct Login {
+    email: String,
+    password: String,
+}
+
+#[derive(Serialize)]
+struct EmptyContext {}
+
+#[post("/login", data = "<login>")]
+/// login route: 
+/// - check login credentials
+/// - redirect back to form if bad credentials
+/// - issue session cookie on good credentials
+/// - redirect to stored location if there is one
+/// - redirect to sign in success page
+fn handle_login(login: Form<Login>) -> Template {
+    Template::render("success", &*login)
+}
+
+#[post("/login", rank = 2)]
+fn login_failed() -> Redirect {
+    Redirect::to(uri!(login))
+}
+
+#[get("/login")]
+/// renders login form
+fn login() -> Template {
+    let context = EmptyContext {};
+    Template::render("login", &context)
+}
+
+#[get("/signup")]
+/// renders signup template
+fn signup() -> Template {
+    let context = EmptyContext {};
+    Template::render("signup", &context)
+}
+
+
 // post /signup, create user, issue cookie on success, redirect
 // get or post to anything else
 //  -> fwd if authenticated
@@ -21,10 +63,13 @@ fn index() -> &'static str {
 //      - check that it represents a valid user
 //      - (maybe) check that the user is authorized to view the resource
 //
-// T_DO
+// ToDO
 // - CSRF prevention
 // - CORS
+// - remember where to redirect 'back to' after redirect to signin
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount("/", routes![index, login, handle_login, signup]).launch();
 }
